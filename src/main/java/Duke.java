2c29cde23f9f4f7bb43f36fccba968ab05529675
Shaun Goh <43333891+SHAUNGOH97@users.noreply.github.com>
2020-02-12 13:00:49 +0800

    buffer commit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -204,6 +204,7 @@ public class Duke extends Application {
         userInput.clear();
     }
 
+
     /**
      * You should have your own function to generate a response to user input.
      * Replace this stub with your completed method.
